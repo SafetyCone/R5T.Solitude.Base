e9deb4b655807b5b85a527a68dd1c5d079352476
MinexAutomation <minex.automation@gmail.com>
2020-02-07 00:59:48 -0800

    Added IVisualStudioSolutionFileOperator service definition interface type.

---
+++ source/R5T.Solitude.Base/Code/Services/Definitions/IVisualStudioSolutionFileOperator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace R5T.Solitude
+{
+    /// <summary>
+    /// A (file-based) Visual Studio solution file operator.
+    /// </summary>
+    public interface IVisualStudioSolutionFileOperator
+    {
+        void CreateNewSolutionFile(string solutionFilePath);
+
+        void AddProjectFile(string solutionFilePath, string projectFilePath, Guid projectTypeGuid);
+
+        bool HasProjectFile(string solutionFilePath, string projectFilePath);
+
+        bool RemoveProjectFile(string solutionFilePath, string projectFilePath);
+
+        /// <summary>
+        /// Lists solution project file references, NOT including any solution folders.
+        /// </summary>
+        IEnumerable<string> ListProjectReferenceFilePaths(string solutionFilePath);
+    }
+}
