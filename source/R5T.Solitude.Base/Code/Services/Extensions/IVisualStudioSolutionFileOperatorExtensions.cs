add2c246da06c3342eb51fef1026e805a473967a
MinexAutomation <minex.automation@gmail.com>
2020-02-10 11:19:35 -0800

    Added IVisualStudioSolutionFileOperator extensions.

---
+++ source/R5T.Solitude.Base/Code/Services/Extensions/IVisualStudioSolutionFileOperatorExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace R5T.Solitude
+{
+    public static class IVisualStudioSolutionFileOperatorExtensions
+    {
+        public static void AddProjectFile(this IVisualStudioSolutionFileOperator visualStudioSolutionFileOperator, string solutionFilePath, string projectFilePath, Guid projectTypeGuid)
+        {
+            var newProjectGuid = Guid.NewGuid();
+
+            visualStudioSolutionFileOperator.AddProjectFile(solutionFilePath, projectFilePath, projectTypeGuid, newProjectGuid);
+        }
+    }
+}
